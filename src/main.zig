cdab561e6f4c73d972e13183fbc5764dd5ea8f72
nua <enuesaa@gmail.com>
2025-10-08 23:55:26 +0900

    feat: サブシェルを起動すると ci が終わらないのでコメントアウトしていたが、そもそも実現したいのはサブシェルの起動なので逆に ci をコメントアウトする

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,6 @@ const wtmp = @import("wtmp");
 
 pub fn main() !void {
     std.debug.print("hello {s}.\n", .{"hello"});
-    // try wtmp.mkdir();
-
-    if (wtmp.isDirExists()) {
-        std.debug.print("dir exists\n", .{});
-    }
-
-    // try wtmp.shell();
+    try wtmp.mkdir();
+    try wtmp.shell();
 }
