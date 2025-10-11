535763db91c4fbdfcd0d9bc36c11a86bbf403f1a
nua <enuesaa@gmail.com>
2025-10-11 18:19:25 +0900

    feat: 左メニューを描画する

--- src/ls.zig
+++ src/ls.zig
@@ -8,6 +8,8 @@ const Model = struct {
     rhs: vxfw.Text,
     children: [1]vxfw.SubSurface = undefined,
     count: u32 = 0,
+    menuItems: [3][]const u8 = [_][]const u8{ "top", "second", "third" },
+    selected: u32 = 0,
 
     pub fn widget(self: *Model) vxfw.Widget {
         return .{
@@ -29,6 +31,18 @@ const Model = struct {
                     ctx.quit = true;
                     return;
                 }
+                if (key.matches('t', .{})) {
+                    if (self.selected > 0) {
+                        self.selected -|= 1;
+                    }
+                    return ctx.consumeAndRedraw();
+                }
+                if (key.matches('b', .{})) {
+                    if (self.selected + 1 < self.menuItems.len) {
+                        self.selected +|= 1;
+                    }
+                    return ctx.consumeAndRedraw();
+                }
                 if (key.matches('a', .{})) {
                     self.count +|= 1;
                     return ctx.consumeAndRedraw();
@@ -40,7 +54,17 @@ const Model = struct {
 
     fn drawFn(ptr: *anyopaque, ctx: vxfw.DrawContext) std.mem.Allocator.Error!vxfw.Surface {
         const self: *Model = @ptrCast(@alignCast(ptr));
-        self.lhs.text = try std.fmt.allocPrint(ctx.arena, "Clicks: {d}", .{self.count});
+
+        self.lhs.text = try std.fmt.allocPrint(ctx.arena, "{s}\n{s}\n{s}\n", .{
+            if (self.selected == 0) "> top" else "  top",
+            if (self.selected == 1) "> second" else "  second",
+            if (self.selected == 2) "> third" else "  third",
+        });
+
+        self.rhs.text = try std.fmt.allocPrint(ctx.arena, "Clicks: {d}", .{self.count});
+
+        self.split.lhs = self.lhs.widget();
+        self.split.rhs = self.rhs.widget();
 
         self.children[0] = .{
             .origin = .{ .row = 1, .col = 1 },
