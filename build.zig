36de9bc7fbcc6470027ff0bfd9893548cf1c0914
nua <enuesaa@gmail.com>
2025-10-25 16:51:11 +0900

    chore: build.zig を見直し

--- build.zig
+++ build.zig
@@ -38,20 +38,19 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     // run
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(b.getInstallStep());
-
+    const run_exe = b.addRunArtifact(exe);
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        run_exe.addArgs(args);
     }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_exe.step);
+    run_exe.step.dependOn(b.getInstallStep());
 
     // test
-    const test_step = b.step("test", "Run tests");
     const test_mod = b.addTest(.{
         .root_module = mod,
     });
-    const test_mod_run = b.addRunArtifact(test_mod);
-    test_step.dependOn(&test_mod_run.step);
+    const run_test_mod = b.addRunArtifact(test_mod);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_test_mod.step);
 }
